5bfee4dd166527e3b0581f16cfe1555307840415
samuka7abr <samuelabrao2006@gmail.com>
2025-01-22 21:33:46 -0300

    feat: update users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -28,6 +28,52 @@ class UsersController {
 
     return response.status(201).json({ message: "Usuário criado com sucesso!" });
   }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user){
+      throw new AppError("Usuário não encontrado!");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este Email já está em uso");
+    }
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password){
+      throw new AppError("vc precisa informar a senha antiga")
+    }
+
+    if(password && old_password){
+      const checkPassword = await compare(old_password, user.password)
+
+      if(!checkPassword){
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8);
+    }
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`, 
+      [user.name, user.email, user.password, id]
+    )
+
+    return response.json()
+  }
 }
+
 
 module.exports = UsersController;
