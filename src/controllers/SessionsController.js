955b80077c096c819f901d57362e480c883a8a9c
samuka7abr <samuelabrao2006@gmail.com>
2025-02-18 11:10:02 -0300

    chore: sessions controller creating auth

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,35 @@
+const authConfig = require('../configs/auth');
+const AppError = require('../utils/AppError');
+const { sign }  = require("jsonwebtoken");
+const knex = require('../database/knex');
+const { compare } = require('bcryptjs');
+
+class SessionsConrtoller {
+    async create(request, response){
+        const { email, password } = request.body;
+
+        const user = await knex("users").where( { email } ).first();
+
+        if(!user){
+            throw new AppError("Email e/ou senha incorreta!, 401");
+        }
+
+        const matchedPassword = compare(password, user.password);
+
+        if(!matchedPassword){
+            throw new AppError("Email e/ou senha incorreta!, 401");
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+        return response.json( { user, token });
+
+    }
+}
+
+module.exports = SessionsConrtoller;
